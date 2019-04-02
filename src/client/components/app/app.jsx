282db668a24dda69cb3ff1d279a18ac637549f76
Mikhail Sementsov <fewebdev@gmail.com>
2019-04-02 21:25:22 +0300

    router

--- src/client/components/app/app.jsx
+++ src/client/components/app/app.jsx
@@ -3,7 +3,9 @@ import {
   Route,
   BrowserRouter as Router,
   Switch,
-  NavLink
+  NavLink,
+  Redirect,
+  HashRouter
 } from "react-router-dom";
 import "./app.scss";
 import Main from "../main";
@@ -16,15 +18,16 @@ class App extends Component {
   state = {};
   render() {
     return (
-      <Router>
+      <HashRouter>
         <Navbar />
         <Switch>
           <Route exact path="/" component={Main} />
           <Route path="/goods" component={Goods} />
           <Route path="/cart" component={Cart} />
           <Route path="/order" component={Order} />
+          <Redirect to="/" />
         </Switch>
-      </Router>
+      </HashRouter>
     );
   }
 }
